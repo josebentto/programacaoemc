ad837b4e9362736de7d731db1ffe77b54f9aefd4
josebentto <josebento.estacio@gmail.com>
2025-02-26 16:17:53 -0400

    Update alunos.c

--- alunos.c
+++ alunos.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main () {
+    int idade, matricula;
+    float altura;
+    char nome [50];
+
+printf ("digite sua idade: \n");
+scanf ("%d", &idade);
+
+printf ("digite sua altura: \n");
+
+scanf ("%f", &altura);
+
+printf("digite o seu nome: \n");
+
+scanf ("%s", &nome);
+
+printf ("digite sua matricula: \n");
+scanf ("%d", &matricula);
+
+printf("Nome do aluno: %c - matricula: %d\n", nome, matricula);
+
+printf("Idade: %d - altura: %F",  idade, altura);
+
+return 0;}
